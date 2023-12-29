a51845267145ea5da91f1352da66e40e4d94ccfb
Marlu <marlupatrocinio01@gmail.com>
2023-12-29 12:01:50 -0300

    price and coins calculate

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ isMachineOn = True
 machineMilk= 1000
 machineCoffee = 1000
 machineWater = 1000
+total = 0
 
 def espresso(machineMilk, machineCoffee, machineWater):
     if machineMilk >= 50 and machineCoffee >= 18 and machineWater >= 50:
@@ -43,14 +44,51 @@ def coinCalculate():
     total = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
     return total
 
+def priceCheck(total, userChoice):
+    if userChoice == "espresso":
+        if total >= 1.50:
+            change = total - 1.50
+            print(f"Here is ${round(change, 2)} in change.")
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
+    elif userChoice == "latte":
+        if total >= 2.50:
+            change = total - 2.50
+            print(f"Here is ${round(change, 2)} in change.")
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
+    elif userChoice == "cappuccino":
+        if total >= 3.00:
+            change = total - 3.00
+            print(f"Here is ${round(change, 2)} in change.")
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
+
 while isMachineOn:
     userChoice = input ("What would you like? (espresso/latte/cappuccino): \n")
     if userChoice == "espresso":
-        machineMilk, machineCoffee, machineWater = espresso(machineMilk, machineCoffee, machineWater)
+        total  = coinCalculate()
+        priceCheck(total, userChoice)
+        if priceCheck(total, userChoice) == True:
+            machineMilk, machineCoffee, machineWater = espresso(machineMilk, machineCoffee, machineWater)
     elif userChoice == "latte":
-        machineMilk, machineCoffee, machineWater = latte(machineMilk, machineCoffee, machineWater)
+        total = coinCalculate()
+        priceCheck(total, userChoice)
+        if priceCheck(total, userChoice) == True:
+            machineMilk, machineCoffee, machineWater = latte(machineMilk, machineCoffee, machineWater)
+
     elif userChoice == "cappuccino":
-        machineMilk, machineCoffee, machineWater = cappuccino(machineMilk, machineCoffee, machineWater)
+        total = coinCalculate()
+        priceCheck(total, userChoice)
+        if priceCheck(total, userChoice) == True:
+            machineMilk, machineCoffee, machineWater = cappuccino(machineMilk, machineCoffee, machineWater)
+
     elif userChoice == "report":
         print(f"Water: {machineWater}ml")
         print(f"Milk: {machineMilk}ml")
